583ce171c6f395ac2b0c9bdfb22166ee177c866d
hezway <hezsystem@gmail.com>
2013-12-05 22:36:02 +0800

    Signed-off-by: hezway <hezsystem@gmail.com>

--- js/schedule.js
+++ js/schedule.js
@@ -1,4 +1,4 @@
-
+//a
 $('#schedulepage').live('pageshow', function(event){
 	var AmbilData;
 	var reg_no = sessionStorage.getItem('reg_no');
@@ -16,7 +16,7 @@ $('#schedulepage').live('pageshow', function(event){
 					  $('#schedulelist').append('<div data-role="collapsible">' + 
 					  					'<h4>[' + loaddata.subject_code +']  '+ loaddata.name_eng + ' / <i>' + loaddata.name_ind + '</i></h4>' +
 					  					'<p>Teacher : <b>' + loaddata.name + '</b> Credit : ' + loaddata.credit + '</p>'+
-										'<p>' + loaddata.day + ' On ' + loaddata.time + ' At ' + loaddata.room + '</p>'+
+										'<p>' + loaddata.day + ' On ' + loaddata.time + ' At ' + loaddata.room + + loaddata.pararel +'</p>'+
 										'<hr></div>');
 					});
 					//$('#schedulelist').listview('refresh');
